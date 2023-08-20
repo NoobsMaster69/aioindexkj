0fabcf5d158c9baf006fecafd602f292c205cf8a
NoobsMaster69 <75945332+NoobsMaster69@users.noreply.github.com>
2023-08-20 18:36:52 +0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Page404Error = () => {
   return (
     <>
-      <div className="flex min-h-full flex-col pt-16 ">
+      <div className="flex min-h-full flex-col pt-16">
         <div className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
           <div className="flex flex-shrink-0 justify-center">
             <Image
-              src={"/LOGO.png"}
+              src={"/LOGO.jpeg"}
               className={
                 "w-28 h-28 rounded-full border-2 border-woodsmoke-50 dark:border-woodsmoke-800"
               }
@@ -22,19 +23,18 @@ const Page404Error = () => {
                 404
               </p>
               <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
-                Waduhhh.
+                Waduhhhh.
               </h1>
               <p className="mt-2 text-base text-gray-500 dark:text-woodsmoke-400">
-                Mau kemana bang.
+                Mauu kemanaa bangg.
               </p>
               <div className="mt-6">
-                <a
-                  href="/home"
-                  className="text-base font-medium text-indigo-600 dark:text-indigo-500 hover:text-indigo-500"
-                >
-                  Go back home
-                  <span aria-hidden="true"> &rarr;</span>
-                </a>
+                <Link href="/" passHref legacyBehavior>
+                  <a className="text-base font-medium text-indigo-600 dark:text-indigo-500 hover:text-indigo-500">
+                    Go back home
+                    <span aria-hidden="true"> &rarr;</span>
+                  </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -43,4 +43,5 @@ const Page404Error = () => {
     </>
   );
 };
+
 export default Page404Error;
